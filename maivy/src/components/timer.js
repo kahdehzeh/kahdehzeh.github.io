472d2ed999df712e92d5551ca228779cc279f170
Kevin Cummings <adfontes@protonmail.com>
2022-12-10 15:09:17 +0100

    refined

--- maivy/src/components/timer.js
+++ maivy/src/components/timer.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from "react"
 
 export const Timer = () => {
 
-    const deadline = new Date("jan 13, 2023 15:00:00").getTime();
+    const deadline = new Date("jan 13, 2023 23:59:59").getTime();
 
     const now = new Date().getTime();
     const t = deadline - now;
@@ -10,16 +10,18 @@ export const Timer = () => {
 
     const happyness = () => {
         if (days >= 30) {
-            return days+" days :(((("
+            return days + " days :(((("
         } else if (days >= 20) {
-            return days+" days :((("
+            return days + " days :((("
         } else if (days >= 10) {
-            return days+" days :(("
-        } else if (days >= 3) {
+            return days + " days :(("
+        } else if (days > 2) {
             return `only ${days - 1} more sleeps :)`
+        } else if (days === 2) {
+            return "Baby is back tomorrow :))"
+        } else if (days === 1) {
+            return "Baby is back today :))))"
         }
-
-
     }
 
 
