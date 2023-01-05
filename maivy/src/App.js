a3b7f95d634fc4ae8d8f2f96812fd2d81a068f48
Kevin Cummings <adfontes@protonmail.com>
2023-01-05 12:08:52 +0100

    click fix

--- maivy/src/App.js
+++ maivy/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Timer } from "./components/timer";
 
 function App() {
   const [visibility, setVisibility] = useState("hidden")
- 
+
 
 
   const clickHandler = () => {
-    visibility==="hidden"?setVisibility("visible"):setVisibility("hidden")
-  }
+    visibility === "hidden" ? setVisibility("visible") : setVisibility("hidden") }
+
+ 
+    
 
   return (
     <div className="App">
       <header className="App-header">
         <h1> How many days until baby is back? </h1>
-        <img src='https://i.imgur.com/75KcV1y.png' alt="egghead" onClick={() => clickHandler()} id="img"></img>        
+        <img src='https://i.imgur.com/75KcV1y.png' alt="egghead" onClick={() => clickHandler()} id="img"></img>
       </header>
       <body style={{ visibility: `${visibility}` }}>
 
@@ -24,8 +26,8 @@ function App() {
 
         </div>
       </body>
-      <footer>        
-        <img src="https://i.imgur.com/TOvrK6s.png" alt="egghead2" id="img2" width={"100px"} style={{visibility:`${visibility}`}} onClick={()=>clickHandler()}></img>
+      <footer id='footer'>
+        <img src="https://i.imgur.com/TOvrK6s.png" alt="egghead2" id="img2" width={"100px"} style={{ visibility: `${visibility}` }} onClick={() => clickHandler()}></img>
       </footer>
 
     </div>
